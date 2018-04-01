ff68e521e63f9ab86ad4ac6961d5368126fbd19e
caio <caio@deb-pc>
2018-04-01 19:47:32 -0300

    [BOOTSTRAP] Dependencies;

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,9 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import wholeState from './reducers/index';
 import thunk from 'redux-thunk';
+import 'jquery';
+import 'bootstrap';
+
 const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
 
 const store = createStore(wholeState, composeEnhancers(applyMiddleware(thunk)));
